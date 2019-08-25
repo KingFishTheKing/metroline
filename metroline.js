649cbf6bc09d0237dd925962a47e8d82b804d8b7
K.Y <K.Y.@not_valid.co.uk>
2019-08-25 21:04:01 +0200

    basic implementation in web component

--- metroline.js
+++ metroline.js
@@ -1,15 +1,48 @@
 class Metroline extends HTMLElement{
     constructor(){
         super();
-        this.optionsObject = this.getAttribute('options') | {};
-        this.dataObject = this.getAttribute('data') | [];
+        this.dataObject = this.getAttribute('stops').split(';');
+        const span = document.createElement('span');
+            this.container = span.cloneNode()
+                this.container.classList.add('metroline-container');
+                this.container.classList.add('r2l');
+            this.row = span.cloneNode()
+                this.row.classList.add('metroline-row');
+            this.item = span.cloneNode();
+                this.item.classList.add('metroline-item')
+            this.display = span.cloneNode()
+                this.display.classList.add('metroline-display');
+            this.stop = span.cloneNode()
+                this.stop.classList.add('metroline-element-stop')
+                this.stop.classList.add('circle')
+            this.route = span.cloneNode()
+                this.route.classList.add('metroline-element-route');
         this.render;
     }
     get render(){
-        let span = document.createElement('span')
-        span.innerText = this.dataObject;
-        const shadow = this.attachShadow({mode: 'closed'})
-            .appendChild(span);
+        const shadow = this.attachShadow({mode: 'open'});
+        let object = this.dataObject.map((obj) => {
+            obj = JSON.parse(obj.replace(/'/gi, "\""));
+            let item = this.item.cloneNode();
+            let display = this.display.cloneNode();
+            let stop = this.stop.cloneNode();
+            stop.setAttribute('info', obj.info);
+            let route = this.route.cloneNode();
+            route.classList.add(obj.status);
+            display.appendChild(route);
+            display.appendChild(stop);
+            item.appendChild(display);
+            return item;
+        });
+        let r = this.row.cloneNode();
+        let c = this.container.cloneNode();
+        r.appendChild(object[0]);
+        r.appendChild(object[1]);
+        r.appendChild(object[2]);
+        r.appendChild(document.getElementById('metroline-style-template').content);
+        c.appendChild(r);
+        
+        shadow.appendChild(c);
     }
 }
 this.customElements.define("metro-line", Metroline);
